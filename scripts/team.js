978f6f56d821ba74314a1ccfcea74ba00c49c114
Kartikay Bhutani <kbhutani0001@gmail.com>
2019-12-22 03:45:43 +0530

    added styling for team page

--- scripts/team.js
+++ scripts/team.js
@@ -21,22 +21,63 @@ let teamData =  [
     Image: "./assets/team/atul.jpeg",
     Facebook: "",
     Linkedin: ""
+  },
+  {
+    Name: "Atul Upadhyay",
+    Position: "sfsf President",
+    Image: "./assets/team/atul.jpeg",
+    Facebook: "",
+    Linkedin: ""
+  },
+  {
+    Name: "Atul Upadhyay",
+    Position: "sfsf President",
+    Image: "./assets/team/atul.jpeg",
+    Facebook: "",
+    Linkedin: ""
+  },
+  {
+    Name: "Atul Upadhyay",
+    Position: "sfsf President",
+    Image: "./assets/team/atul.jpeg",
+    Facebook: "",
+    Linkedin: ""
+  },
+  {
+    Name: "Atul Upadhyay",
+    Position: "sfsf President",
+    Image: "./assets/team/atul.jpeg",
+    Facebook: "",
+    Linkedin: ""
   }
 
 ]
 teamDiv = document.getElementsByClassName("teamImageDiv")[0]
 
 for(var i=0;i<teamData.length;i++){
-text = `
-  <div class="teamMember">
-    <img class="teamImage" src="${teamData[i].Image}" alt="">
-    <div class="teamDetails">
-      <h5><b>${teamData[i].Name}</b></h5>
-      <h5>${teamData[i].Position}</h5>
-    </div>
-  </div>
+
+
+text = 
 
 `
+<div class="teamMember">
+<div class="teamSocialHandle">
+<img class="teamImage" src="${teamData[i].Image}" alt="">
+<div class="teamSocialLinks">
+  <a href="#"><img  class="facebook" src="./assets/facebook-logo.svg"/></a>
+  <a href="#"><img  class="linkedin" src="./assets/linkedin-logo.svg"/></a>
+</div>
+</div>
+<div class="teamDetails">
+  <h5><b>${teamData[i].Name}</b></h5>
+  <h5>${teamData[i].Position}</h5>
+</div>
+</div>
+`
+
+
+
+
 teamDiv.innerHTML = teamDiv.innerHTML + text
 
 
