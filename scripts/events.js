f46e541d8e4b8f114ff1f74907a241a5a0b0ab70
abhinavsri360 <abhinav.022000@gmail.com>
2019-12-27 21:54:06 +0530

    events data file created

---
+++ scripts/events.js
@@ -0,0 +1,13 @@
+let eventData =
+[
+	{
+		image:"./assets/Events/culture.jpeg",
+		venue:"JIIT 128",
+		title:"Envictus Eventus",
+		date:"2020-02-01",
+		time:"00:00:00",
+		description:"Description of the event",
+		rules:"Here's the rules which you ain't like",
+		prizes:"$1,00,000"
+	}
+]
