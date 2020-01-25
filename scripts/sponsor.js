e5537ddf4ca4fc6b9f4155640ed89ad901c94909
Abhinav Srivastava <44001310+abhinavsri360@users.noreply.github.com>
2020-01-25 23:58:39 +0530

    more sponsors (#91)
    * IAS
    
    * ACE
    
    * gamers
    
    * doctor
    
    * sponsor data added
    
    * rog
    
    * KTM
    
    * whooosh
    
    * 29.jpg
    
    * data
    
    * anytime
    
    * images

--- scripts/sponsor.js
+++ scripts/sponsor.js
@@ -2,7 +2,7 @@ let powereddata=[
   {
     Name: "Grog",
     for: "Powered By",
-    Image: "./assets/sponsor/grog.png",
+    Image: "./assets/sponsor/9.jpg",
     link:'./'
   },
    {
@@ -18,7 +18,7 @@ let powereddata=[
   {
     Name: "KTM",
     for: "Associate Partner",
-    Image: "./assets/sponsor/KTM.png",
+    Image: "./assets/sponsor/4.jpg",
     link:'https://www.ktm.com/'
   }
 ]
@@ -27,34 +27,52 @@ let sponsordata =  [
   {
     Name: "Woosh",
     for: "Fashion Partner",
-    Image: "./assets/sponsor/WhooshBlack.png",
+    Image: "./assets/sponsor/36.jpg",
     link:"https://whoosh.com/"
   },
   {
     Name: "PrepBytes",
     for: "Technical Partner",
-    Image: "./assets/sponsor/prepbytes (2).png",
+    Image: "./assets/sponsor/29.jpg",
     link:'https://www.prepbytes.com/'
 
   },
    {
     Name: "Data Trained",
     for: "Training Partner",
-    Image: "./assets/sponsor/atatrained.png",
+    Image: "./assets/sponsor/1.jpg",
     link:"https://www.datatrained.com/"
   },
   {
     Name: "Anytime Fitness",
     for: "Gyming Partner",
-    Image: "./assets/sponsor/AnytimeFitnessLogo.png",
+    Image: "./assets/sponsor/2.jpg",
     link:'https://www.anytimefitness.com/'
   },
-  // {
-  //   Name: "IAS Academy",
-  //   for: "Education Partner",
-  //   Image: "./assets/sponsor/atatrained.png"
-
-  // }
+  {
+    Name: "IAS Academy",
+    for: "Education Partner",
+    Image: "./assets/sponsor/27.jpg",
+    link: 'https://www.nextias.com/'
+ },
+ {
+   Name: "ACE Engineering",
+   for: "Learning Partner",
+   Image: "./assets/sponsor/23.jpg",
+   link: 'https://www.aceenggacademy.com/'
+ },
+  {
+    Name: "Gamers Sanctuary",
+    for: "Gaming Partner",
+    Image: "./assets/sponsor/15.jpg",
+    link: './'
+  },
+  {
+    Name: "Doctor Titoria",
+    for: "Medical Partner",
+    Image: "./assets/sponsor/21.jpg",
+    link: './'
+  }
 
 ]
 
